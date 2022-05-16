16c05f873b6374480e59193799a98bd3147fa0f5
bajenova <105018390+bajenova@users.noreply.github.com>
2022-05-16 14:15:49 -0700

    Add files via upload

---
+++ Difficulty.java
@@ -0,0 +1,65 @@
+public enum Difficulty {
+	
+	EASY {
+      public int rowCount {
+         return 8;
+      }
+      
+      public int colCount {
+         return 10;
+      }
+      
+		@SuppressWarnings("unused")
+		public int getNumberOfMines() {
+			return 10;
+		}
+		
+		public String toString() {
+			return "Easy";
+		}
+		
+	},
+   
+	NORMAL {
+      public int rowCount {
+         return 14;
+      }
+      
+      public int colCount {
+         return 18;
+      }
+      
+		@SuppressWarnings("unused")
+		public int getNumberOfMines() {
+			return 40;
+		}
+		
+		public String toString() {
+			return "Normal";
+		}
+		
+	},
+   
+	HARD {
+      public int rowCount {
+         return 20;
+      }
+      
+      public int colCount {
+         return 24;
+      }
+		
+		@SuppressWarnings("unused")
+		public int getNumberOfMines() {
+			return 99;
+		}
+		
+		public String toString() {
+			return "Hard";
+		}
+		
+	};
+	
+	abstract int getNumberOfMines();
+	abstract int getTimelimit();
+}
