6b39a75e60ca8f3c909bc40592f4a910c3d8e7bf
etlindquist <105020527+etlindquist@users.noreply.github.com>
2022-05-26 13:39:02 -0700

    Update Difficulty.java

--- Difficulty.java
+++ Difficulty.java
@@ -1,62 +1,122 @@
-public class Difficulty extends Gui {
-   public int difficulty;
+/*public enum Difficulty {
 	
-   public int rowCount(int difficulty) {
-      if (difficulty == 0) {
+	EASY {
+		public int rowCount() {
+			return 8;
+		}
+
+		public int colCount() {
+			return 10;
+		}
+
+		public int tileSize() {
+			return 50;
+		}
+      
+		public int getNumberOfMines() {
+			return 10;
+		}
+		
+		public String toString() {
+			return "Easy";
+		}
+		
+	},
+   
+	NORMAL {
+		public int rowCount() {
+			return 14;
+		}
+      
+		public int colCount() {
+			return 18;
+		}
+      
+		public int tileSize() {
+			return 40;
+		}
+      
+		public int getNumberOfMines() {
+			return 40;
+		}
+		
+		public String toString() {
+			return "Normal";
+		}
+		
+	},
+   
+	HARD {
+		public int rowCount() {
+			return 20;
+		}
+      
+		public int colCount() {
+			return 24;
+		}
+      
+		public int tileSize() {
+			return 30;
+		}
+		
+		public int getNumberOfMines() {
+			return 99;
+		}
+		
+		public String toString() {
+			return "Hard";
+		}
+		
+	};
+	
+	abstract int getNumberOfMines();
+}*/
+public class Difficulty{
+   public String diff;
+   public Difficulty(){
+      diff = "Normal";
+   }
+   //Easy, Normal, or Hard
+   public void newDiff(String newDiff){
+      diff = newDiff;
+   }
+   public int rowCount(){
+      if(diff.equals("Easy")){
          return 8;
       }
-      if (difficulty == 1) {
+      if(diff.equals("Normal")){
          return 14;
-      } 
-      if (difficulty == 2) {
-         return 20;
       }
+      return 20;
    }
-      
-   public int colCount(int difficulty) {
-      if (difficulty == 0) {
+   public int colCount(){
+      if(diff.equals("Easy")){
          return 10;
       }
-      if (difficulty == 1) {
+      if(diff.equals("Normal")){
          return 18;
-      } 
-      if (difficulty == 2) {
-         return 24;
-      }   }
-      
-   public int tileSize(int difficulty) {
-	if (difficulty == 0) {
+      }
+      return 24;
+   }
+   public int tileSize(){
+      if(diff.equals("Easy")){
          return 50;
       }
-      if (difficulty == 1) {
+      if(diff.equals("Normal")){
          return 40;
-      } 
-      if (difficulty == 2) {
-         return 30;
       }
+      return 30;
    }
-      
-   public int getNumberOfMines(int difficulty) {
-	if (difficulty == 0) {
+   public int getNumberOfMines(){
+      if(diff.equals("Easy")){
          return 10;
       }
-      if (difficulty == 1) {
+      if(diff.equals("Normal")){
          return 40;
-      } 
-      if (difficulty == 2) {
-         return 99;
       }
+      return 99;
    }
-		
-   public String toString(int difficulty) {
-	if (difficulty == 0) {
-         return "Easy";
-      }
-      if (difficulty == 1) {
-         return "Normal";
-      } 
-      if (difficulty == 2) {
-         return "Hard";
-      }
+   public String toString(){
+      return diff;
    }
 }
