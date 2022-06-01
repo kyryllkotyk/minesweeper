ae3128f02e77eb50b29507587fc8692b990ddcd7
bajenova <105018390+bajenova@users.noreply.github.com>
2022-06-01 13:05:16 -0700

    Update NewGui.java

--- NewGui.java
+++ NewGui.java
@@ -56,4 +56,56 @@ public class NewGui extends JPanel{
       }*/
 
    }
+   //jbuttons
+	public JButton[][] buttons = new JButton[diff.colCount()][diff.rowCount()];
+   buttons[x1][y1] = new JButton;
+   JPanel buttonPanel;
+   //finish making buttons out of coordinates. figure out how to do this.
+   
+   public boolean revealed[][];
+   public void revealTile(int bombsAdjacent(x1, y1)) {
+      revealed[x1][y1] = true;
+      //sets background color to beige or sand dollar
+      buttons[x1][y1].setBackground(245, 245, 220);
+      if((x1+y1)%2==0) {
+         buttons[x1][y1].setBackground(194, 178, 128);
+      }
+      if(bombs[x1][y1] == true) { //checks if bomb
+         buttons[x1][y1].setBackground(Color.RED);
+         buttons[x1][y1].setText("M"); //change to image later?
+      } else if (bombsAdjacent > 0) { 
+            //go through each value of bombs adjacent to assign foreground color
+            if (bombsAdjacent == 1) {
+               buttons[x1][y1].setForeground(Color.BLUE); 
+            }
+            if (bombsAdjacent == 2) {
+               buttons[x1][y1].setForeground(Color.GREEN); 
+            }
+            if (bombsAdjacent == 3) {
+               buttons[x1][y1].setForeground(Color.RED); 
+            }
+            if (bombsAdjacent == 4) {
+               buttons[x1][y1].setForeground(Color.PURPLE); 
+            }
+            if (bombsAdjacent == 5) {
+               buttons[x1][y1].setForeground(Color.WHITE);
+            }
+            if (bombsAdjacent == 6) {
+               buttons[x1][y1].setForeground(Color.WHITE);
+            }
+            if (bombsAdjacent == 7) {
+               buttons[x1][y1].setForeground(Color.WHITE);
+            }
+            if (bombsAdjacent == 8) {
+               buttons[x1][y1].setForeground(Color.WHITE);
+            }
+            buttons[x1][y1].setText(bombsAdjacent(x1, y1));
+      } else { //nothing; make content blank
+         buttons[x1][y1].setText("");
+      }
+   }
+   
+   public boolean isRevealed(x1, y1) {
+      return revealed[x1][y1];
+   }
 }
