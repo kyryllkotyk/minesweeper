346d589fdb68f43f590b2b45d2fffd867374b29d
etlindquist <105020527+etlindquist@users.noreply.github.com>
2022-06-01 12:49:48 -0700

    Update NewGui.java

--- NewGui.java
+++ NewGui.java
@@ -8,6 +8,7 @@ public class NewGui extends JPanel{
    private Difficulty diff;
    private int x;
    private int y;
+   private int grid[][];
    public NewGui(Difficulty dif){
       diff = dif;
       x=0;
@@ -22,25 +23,37 @@ public class NewGui extends JPanel{
       x=xNew;
       y=yNew;
    }
+   public void updateGrid(int[][] tiles){
+      grid = tiles;
+   }
    public void paintComponent(Graphics g){
       super.paintComponent(g);
       Color evenSquares = new Color(0,255,0);
       Color oddSquares = new Color(0,168,0);
       Color background = new Color(0,100,0);
       int tile = diff.tileSize();
-      g.setColor(background);
-      g.fillRect(0,0,x,y);
       g.setColor(oddSquares);
-      g.fillRect(0,60,x,y-60);
+      g.fillRect(0,0,x,y-60);
       g.setColor(evenSquares);
       for(int i = 0; i<diff.colCount(); i++){
          for(int j = 0; j<diff.rowCount(); j++){
             if(((i+j)%2)==0){
                int x1 = i*tile;
-               int y1 = j*tile+60;
+               int y1 = j*tile;
                g.fillRect(x1,y1,tile,tile);
             }
          }
       }
+      /*for(int i = 0; i<grid.length; i++){
+         for(int j = 0; j<grid[i].length; j++){
+            if(i%2==j%2){
+               
+            }
+            else{
+               
+            }
+         }
+      }*/
+
    }
 }
