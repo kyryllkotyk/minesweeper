71cdb5eaba5b0f73414472590128a373ce86f9eb
bajenova <105018390+bajenova@users.noreply.github.com>
2022-06-02 13:46:26 -0700

    Update NewGui.java

--- NewGui.java
+++ NewGui.java
@@ -75,4 +75,69 @@ public class NewGui extends JPanel{
          }
       }
    }
+   
+   JPanel buttonPanel;
+   public JButton[][] buttons = new JButton[diff.colCount()][diff.rowCount()];
+   public void makeButtons() {
+      //rows
+   	for (int y = 0; y < diff.rowCount(); y++) {
+   	   //cols
+   	   for (int x = 0; x < diff.colCount(); x++) {
+   		   //button name (x,y)
+   		   buttons[x][y].setName(Integer.toString(x) + "," + Integer.toString(y));
+   		   //add mouseListener, change to mousepoint gui thing
+   		   buttons[x][y].addMouseListener(this);
+            //button txt
+            //if revealed == true -> show text, if revealed == false -> null
+   		   buttons[x][y] = new JButton(); //add text once revealed? will need to access both bombs boolean and bombsAdjacent
+         }
+      }
+   }
+   
+   public boolean revealed[][];
+   public void revealTile(int bombsAdjacent(int x1, int y1)) {
+      revealed[x1][y1] = true;
+      //sets background color to beige or sand dollar
+      buttons[x1][y1].setBackground(245, 245, 220);
+      if((x1+y1)%2==0) {
+         buttons[x1][y1].setBackground(194, 178, 128);
+      }
+      if(bombs[x1][y1] == true) { //checks if bomb
+         buttons[x1][y1].setBackground(Color.RED);
+         buttons[x1][y1].setText("M"); //change to image later?
+      } else if (bombsAdjacent > 0) { 
+            //go through each value of bombs adjacent to assign foreground color
+            if (bombsAdjacent == 1) {
+               buttons[x1][y1].setForeground(Color.one); 
+            }
+            if (bombsAdjacent == 2) {
+               buttons[x1][y1].setForeground(Color.two); 
+            }
+            if (bombsAdjacent == 3) {
+               buttons[x1][y1].setForeground(Color.three); 
+            }
+            if (bombsAdjacent == 4) {
+               buttons[x1][y1].setForeground(Color.four); 
+            }
+            if (bombsAdjacent == 5) {
+               buttons[x1][y1].setForeground(Color.five);
+            }
+            if (bombsAdjacent == 6) {
+               buttons[x1][y1].setForeground(Color.six);
+            }
+            if (bombsAdjacent == 7) {
+               buttons[x1][y1].setForeground(Color.seven);
+            }
+            if (bombsAdjacent == 8) {
+               buttons[x1][y1].setForeground(Color.eight);
+            }
+            buttons[x1][y1].setText(bombsAdjacent(x1, y1));
+      } else { //nothing; make content blank
+         buttons[x1][y1].setText("");
+      }
+   }
+   
+   public boolean isRevealed(int x1, int y1) {
+      return revealed[x1][y1];
+   }
 }
