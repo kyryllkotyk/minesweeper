3a618ce827fdf0f6c657e38c53e9457f21445b55
etlindquist <105020527+etlindquist@users.noreply.github.com>
2022-06-02 14:55:13 -0700

    Update NewGui.java

--- NewGui.java
+++ NewGui.java
@@ -29,18 +29,18 @@ public class NewGui extends JPanel{
    }
    public void paintComponent(Graphics g){
       super.paintComponent(g);
-      Color evenSquares = new Color(0,100,0);
-      Color oddSquares = new Color(0,168,0);
-      Color lightGray = new Color(204, 204, 204);
+      Color evenSquares = new Color(28,119,34);
+      Color oddSquares = new Color(11,152,20);
+      Color lightGray = new Color(198, 206, 123);
       Color gray = new Color(153, 153, 153);
-      Color one = new Color(10,100,255);
-      Color two = new Color(10,100,255);
-      Color three = new Color(10,100,255);
-      Color four = new Color(10,100,255);
-      Color five = new Color(10,100,255);
-      Color six = new Color(10,100,255);
-      Color seven = new Color(10,100,255);
-      Color eight = new Color(10,100,255);
+      Color one = new Color(4,56,226);
+      Color two = new Color(22,129,22);
+      Color three = new Color(249,10,0);
+      Color four = new Color(129,3,117);
+      Color five = new Color(255,128,0);
+      Color six = new Color(55,185,172);
+      Color seven = new Color(0,0,0);
+      Color eight = new Color(82,97,95);
       int tile = diff.tileSize();
       g.setColor(oddSquares);
       g.fillRect(0,0,x,y-60);
@@ -70,74 +70,33 @@ public class NewGui extends JPanel{
             }
             if(grid[i][j]!=0){
                g.fillRect(i*tile,j*tile,tile,tile);
+               if(grid[i][j] == 1){
+                  g.setColor(one);
+               }
+               else if(grid[i][j] == 2){
+                  g.setColor(two);
+               }
+               else if(grid[i][j] == 3){
+                  g.setColor(three);
+               }
+               else if(grid[i][j] == 4){
+                  g.setColor(four);
+               }
+               else if(grid[i][j] == 5){
+                  g.setColor(five);
+               }
+               else if(grid[i][j] == 6){
+                  g.setColor(six);
+               }
+               else if(grid[i][j] == 7){
+                  g.setColor(seven);
+               }
+               else if(grid[i][j] == 8){
+                  g.setColor(eight);
+               }
                g.drawString(""+grid[i][j],i*tile+tile/3,j*tile+tile-10);
             }
          }
       }
-   }
-   
-   JPanel buttonPanel;
-   public JButton[][] buttons = new JButton[diff.colCount()][diff.rowCount()];
-   public void makeButtons() {
-      //rows
-   	for (int y = 0; y < diff.rowCount(); y++) {
-   	   //cols
-   	   for (int x = 0; x < diff.colCount(); x++) {
-   		   //button name (x,y)
-   		   buttons[x][y].setName(Integer.toString(x) + "," + Integer.toString(y));
-   		   //add mouseListener, change to mousepoint gui thing
-   		   buttons[x][y].addMouseListener(this);
-            //button txt
-            //if revealed == true -> show text, if revealed == false -> null
-   		   buttons[x][y] = new JButton(); //add text once revealed? will need to access both bombs boolean and bombsAdjacent
-         }
-      }
-   }
-   
-   public boolean revealed[][];
-   public void revealTile(int bombsAdjacent(int x1, int y1)) {
-      revealed[x1][y1] = true;
-      //sets background color to beige or sand dollar
-      buttons[x1][y1].setBackground(245, 245, 220);
-      if((x1+y1)%2==0) {
-         buttons[x1][y1].setBackground(194, 178, 128);
-      }
-      if(bombs[x1][y1] == true) { //checks if bomb
-         buttons[x1][y1].setBackground(Color.RED);
-         buttons[x1][y1].setText("M"); //change to image later?
-      } else if (bombsAdjacent > 0) { 
-            //go through each value of bombs adjacent to assign foreground color
-            if (bombsAdjacent == 1) {
-               buttons[x1][y1].setForeground(Color.one); 
-            }
-            if (bombsAdjacent == 2) {
-               buttons[x1][y1].setForeground(Color.two); 
-            }
-            if (bombsAdjacent == 3) {
-               buttons[x1][y1].setForeground(Color.three); 
-            }
-            if (bombsAdjacent == 4) {
-               buttons[x1][y1].setForeground(Color.four); 
-            }
-            if (bombsAdjacent == 5) {
-               buttons[x1][y1].setForeground(Color.five);
-            }
-            if (bombsAdjacent == 6) {
-               buttons[x1][y1].setForeground(Color.six);
-            }
-            if (bombsAdjacent == 7) {
-               buttons[x1][y1].setForeground(Color.seven);
-            }
-            if (bombsAdjacent == 8) {
-               buttons[x1][y1].setForeground(Color.eight);
-            }
-            buttons[x1][y1].setText(bombsAdjacent(x1, y1));
-      } else { //nothing; make content blank
-         buttons[x1][y1].setText("");
-      }
-   }
-   
-   public boolean isRevealed(int x1, int y1) {
-      return revealed[x1][y1];
    }
 }
