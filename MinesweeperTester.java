509ba5a6f0aa16159f4a94ee31ea315ff713ba60
kyryllkotyk <105020476+kyryllkotyk@users.noreply.github.com>
2022-06-06 22:21:14 -0700

    Update MinesweeperTester.java

--- MinesweeperTester.java
+++ MinesweeperTester.java
@@ -4,21 +4,7 @@ import java.awt.event.*;
 
 public class MinesweeperTester{
    public static void main(String[] args){
-      //JFrame frame = new JFrame();
       Difficulty diff = new Difficulty();
-      /*int sizeX = diff.colCount()*diff.tileSize();
-      int sizeY = diff.rowCount()*diff.tileSize()+60;
-      frame.setSize(sizeX+11, sizeY);
-      frame.setTitle("Minesweeper");
-      frame.setLayout(new BorderLayout());
-      JPanel north = new JPanel(new FlowLayout());
-      north.add(new JLabel("flags:"+12+"     time:"+40));
-      frame.add(north, BorderLayout.NORTH);
-      NewGui gui = new NewGui();
-      gui.setSize(sizeX,sizeY);
-      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      frame.add(gui, BorderLayout.CENTER);
-      frame.setVisible(true);*/
       GuiController gui = new GuiController(diff);
       Time t = new Time();
       gui.runGame();
