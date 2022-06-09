103974d473e614991ee2b5ce40c4ff83aa61af1e
etlindquist <105020527+etlindquist@users.noreply.github.com>
2022-06-08 20:13:32 -0700

    Update Time.java

--- Time.java
+++ Time.java
@@ -3,9 +3,9 @@ import java.util.*;
 public class Time{
    public int time;
    private Timer clock;
-   public Time(){
-      Timer clock = new Timer();
-      time = 0;
+   private NewGui gui;
+   public Time(NewGui GUI){
+      gui = GUI;
    }
    //Returns the value of the time int
    public int getTime(){
@@ -13,6 +13,7 @@ public class Time{
    }
    //Resets the time value and starts the clock
    public void newGame(){
+      Timer clock = new Timer();
       time = 0;
       clock.scheduleAtFixedRate(task, 1000, 1000);
    }
@@ -20,6 +21,8 @@ public class Time{
    TimerTask task = new TimerTask(){
       public void run(){
          time++;
+         gui.setTime(time);
+         gui.repaint();
       }
    };
 }
