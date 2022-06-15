487bcef756094ba58617b419b82ef829cd74e722
etlindquist <105020527+etlindquist@users.noreply.github.com>
2022-06-14 20:21:27 -0700

    Update MinesweeperTester.java

--- MinesweeperTester.java
+++ MinesweeperTester.java
@@ -6,7 +6,6 @@ public class MinesweeperTester{
    public static void main(String[] args){
       Difficulty diff = new Difficulty();
       GuiController gui = new GuiController(diff);
-      Time t = new Time();
       gui.runGame();
    }
 }
